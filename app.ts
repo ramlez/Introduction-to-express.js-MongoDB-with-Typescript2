9ccee829b0cffe76ff55134b148f2b68b61c2960
ramlez <m.grodek@gmail.com>
2016-12-11 22:53:21 +0100

    Basic HTTP server

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("Hello Reset ATH!");
+import http = require("http");
+
+http.createServer((req, res) => {
+    console.log(req);
+
+    res.end(new Date().valueOf().toString());
+
+}).listen(3000, () => {
+    console.log("Server is listening on 3000 port");
+});
